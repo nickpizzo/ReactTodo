3343678d8ad0bd72e1fda3f79d334c88e1461a8c
Nick Pizzo <nick.pizzo@gmail.com>
2016-08-08 17:14:21 -0700

    fixed git revert error

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,42 +1,50 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id: 1,
-          text: 'walk the dog'
+          text: 'Walk the dog'
         }, {
           id: 2,
-          text: 'clean the yard'
+          text: 'Clean the yard'
         }, {
           id: 3,
-          text: 'mow the grass'
+          text: 'Leave mail on porch'
         }, {
           id: 4,
-          text: 'do the dishes'
+          text: 'Play video games'
         }
       ]
     };
   },
-
   handleAddTodo: function (text) {
     alert('new todo: ' + text);
   },
-
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function () {
     var {todos} = this.state;
 
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
   }
-})
+});
 
 module.exports = TodoApp;
