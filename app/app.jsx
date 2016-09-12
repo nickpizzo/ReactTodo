006abc24290b802d786e2c8abb1670f4bb10297b
Nick Pizzo <nick.pizzo@gmail.com>
2016-09-11 19:08:11 -0700

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-//^^used ES6 destructuring syntax to pull off four variables from react router
 
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New State', store.getState())
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
+
 
 // Load foundation
 $(document).foundation();
